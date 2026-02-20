11c1818a7aab8c1e099b3fb39bcbfee2fb1d4eba
TheGianMaster <gian.marco.marinelli97@gmail.com>
2026-02-20 16:26:58 +0100

    TK.link01 - customers logos, redirection to their sites

--- src/components/ClientsGrid.tsx
+++ src/components/ClientsGrid.tsx
@@ -11,13 +11,13 @@ type Client = {
 };
 
 const CLIENTS: Client[] = [
-    { id: "c1", name: "Devit", href: "https://finance.yahoo.com"},
-    { id: "c2", name: "Dropcam", href: "https://schulzitalia.com", logoSrc: "/clients/schulz.png" },
-    { id: "c3", name: "Centro Idea Casa", href: "https://www.centroideacasa.it/", logoSrc: "/clients/cic.png"},
-    { id: "c4", name: "Edil Iomida", href: "https://www.amazon.com"},
-    { id: "c5", name: "Centro Airone", href: "https://www.amd.com"},
-    { id: "c6", name: "Billy's", href: "https://www.cisco.com" },
-    { id: "c7", name: "Cliente X", href: "https://example.com" },
+    { id: "c1", name: "Devit", href: "https://www.devit.cloud/", logoSrc: "/clients/logo_devit.png" },
+    { id: "c2", name: "Schulz", href: "https://schulzitalia.com", logoSrc: "/clients/logo_schulz.png" },
+    { id: "c3", name: "Centro Idea Casa", href: "https://www.centroideacasa.it/", logoSrc: "/clients/logo_centroideacasa.png"},
+    { id: "c4", name: "Ediljomida", href: "https://ediljomida.it/", logoSrc: "/clients/logo_ediljomida.png" },
+    { id: "c5", name: "Centro Airone", href: "https://www.instagram.com/centroolistico_airone/", logoSrc: "/clients/logo_airone.png" },
+    { id: "c6", name: "Billy's", href: "https://www.instagram.com/billys_ristopub/", logoSrc: "/clients/logo_billys.png" },
+    { id: "c7", name: "Eurometal", href: "https://www.eurometalvalmontone.it/", logoSrc: "/clients/logo_eurometal.png"  },
     { id: "c7", name: "Cliente Y", href: "https://example.com" },
     { id: "c7", name: "Cliente Z", href: "https://example.com" },
     { id: "c7", name: "Cliente W", href: "https://example.com" },
